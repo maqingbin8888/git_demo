1997ffaec042ced4725319dcb83ca52ec89316a0
maqingbin8888 <411306879@qq.com>
2015-12-13 00:33:37 +0800

    aaa

--- git_demo/src/git_demo/HelloWorld.java
+++ git_demo/src/git_demo/HelloWorld.java
@@ -3,6 +3,6 @@ package git_demo;
 public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("aaabbbCC");
-		System.out.println("aaabbbCCdddd");
+		System.out.println("aaabbbCCddddeee");
 	}
 }
