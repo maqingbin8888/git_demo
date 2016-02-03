6ad9accef4e9cdfbe6cab3f30626356be4b79706
maqingbin8888 <411306879@qq.com>
2016-02-03 11:27:12 +0800

    Update HelloWorld.java

--- git_demo/src/git_demo/HelloWorld.java
+++ git_demo/src/git_demo/HelloWorld.java
@@ -4,5 +4,6 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("aaabbbCC");
 		System.out.println("aaabbbCCddddeee");
+		System.out.println("aaabbbCCddddeee");
 	}
 }
