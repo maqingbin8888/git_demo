5450e0c3c1fee614c66100ed0d667ef130683159
maqingbin8888 <411306879@qq.com>
2016-02-15 22:14:02 +0800

    201602152214

--- git_demo/src/git_demo/HelloWorld.java
+++ git_demo/src/git_demo/HelloWorld.java
@@ -4,6 +4,6 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("aaabbbCC");
 		System.out.println("aaabbbCCddddeee");
-		System.out.println("masterupdate201602142213");
+		System.out.println("masterupdate201602152214");
 	}	
 }
